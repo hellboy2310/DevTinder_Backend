45780a6fd8a6e88571e17d36c49a223e5604898b
bhavesh2310 <bhavesh.purohit@o2h.com>
2025-04-05 14:10:09 +0530

    feat: exploring get post delete api

--- src/app.js
+++ src/app.js
@@ -2,12 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test', (req, res) => {
-    res.send('hello from test server');
+// app.use('/test', (req, res) => {
+//     res.send('hello from test server');
+// })
+
+// app.use('/hello', (req, res) => {
+//     res.send('hello from a normal user');
+// })
+
+app.get('/user', (req, res) => {
+    res.send({
+        firstName: 'bhavesh',
+        lastName: 'purohit'
+    })
 })
 
-app.use('hello', (req, res) => {
-    res.send('hello from a normal user');
+app.post('/user', (req, res) => {
+    res.send({
+        message: `data saved successfully`
+    })
 })
 
 app.listen(3000, () => {
